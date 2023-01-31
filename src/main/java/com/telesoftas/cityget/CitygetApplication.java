310e805792f2bd324352a74933126365d579ddef
Jerry <jerry.causby@spirent.com>
2023-01-31 11:26:48 +0200

    Initial commit

---
+++ src/main/java/com/telesoftas/cityget/CitygetApplication.java
@@ -0,0 +1,13 @@
+package com.telesoftas.cityget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitygetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitygetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telesoftas/cityget/CitygetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telesoftas.cityget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitygetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
